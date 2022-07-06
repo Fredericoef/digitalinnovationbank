007c2018a4babf98aea1fda3d269704f8090724f
Fredericoef <frederico.ef@gmail.com>
2022-07-05 21:50:09 -0300

    primeiro commit

---
+++ src/main/kotlin/one.digitalinnovation.digitalonebank/Pessoa.kt
@@ -0,0 +1,14 @@
+
+
+
+class Pessoa {
+    var nome: String = "Josefino"
+    var cpf: String = "032.878.254-12"
+}
+
+fun main() {
+    val josefino = Pessoa()
+
+    println(josefino.nome)
+    println(josefino.cpf)
+}
